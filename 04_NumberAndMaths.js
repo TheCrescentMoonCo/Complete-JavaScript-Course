98fad43823aee105c26d6f8e8a72310e80d72c4e
TheCrescentMoonCo <TheCrescentMoonCo.rediffmail.com>
2026-01-01 16:28:42 +0530

    Number and Maths in JavaScript

---
+++ 04_NumberAndMaths.js
@@ -0,0 +1,33 @@
+// Numbers and Maths in JavaScript
+
+// Numbers
+
+const score = 400;
+console.log(score);
+
+const balance = new Number(1000);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const decimalNumberOne = 123.8960;
+console.log(decimalNumberOne.toPrecision(3));
+
+const hundreds = 1000000;
+
+console.log(hundreds.toLocaleString('en-IN'));
+
+// Maths
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.5));
+console.log(Math.floor(4.5));
+console.log(Math.min(4, 5, 1, 3));
+console.log(Math.max(4, 5, 1, 3));
+console.log(Math.random());     // Gives an random number between 0 and 1 as an output
+
+
+
