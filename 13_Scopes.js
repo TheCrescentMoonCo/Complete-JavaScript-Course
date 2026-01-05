d06d2fa37109c20de14ae40c159330162ade6a0b
TheCrescentMoonCo <TheCrescentMoonCo.rediffmail.com>
2026-01-05 11:30:17 +0530

    Scopes in JavaScript

---
+++ 13_Scopes.js
@@ -0,0 +1,33 @@
+// Global and Local Scopes in JavaScript
+
+// Global Scope
+const a = 12;
+let b = 10;
+var c = 100;
+
+if(true) {      // Local Scope
+    const a = 1;
+    let b = 2;
+    var c = 3;
+}
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+function one() {
+    const username = 'Donald Trumph';
+
+    function two(){
+        const website = 'google.com'
+        console.log(username);
+    }
+    // console.log(website);
+    
+    two();
+}
+
+one();
+
+// Function expression(when function is stored in a variable) has no Hoisting power.
+ 
