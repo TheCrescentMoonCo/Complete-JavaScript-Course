9d54fb0f6882c91bdcf97fda455fddce9ba05b37
TheCrescentMoonCo <TheCrescentMoonCo.rediffmail.com>
2026-01-06 11:52:13 +0530

    Iterations in JavaScript

--- 18_Iterations.js
+++ 18_Iterations.js
@@ -1,6 +1,8 @@
 // Iterations in JavaScript
 
-// for loop
+// for loop, while loop, do-while loop
+
+// For Loop
 
 for(let j = 0; j <= 10; j++) {
     console.log(j);
@@ -27,3 +29,21 @@ for(let i = 0; i < nameOfHeroes.length; i++) {
 // Break throws out of loop scope
 // Continue skips the current iteration and moves onto the next iteration
 
+
+// While Loop
+
+let index = 0;
+while(index <= 100) {
+    console.log(index);
+    index = index + 2;
+}
+
+
+// Do-While Loop
+
+let score = 1;
+
+do {
+    console.log(score);
+    score++;
+} while(score <= 10);
